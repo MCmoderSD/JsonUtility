ad32e0fe2c7f8900b01b7f5aee82d5910acff67a
Seraphin Berger <MCmoderSD@web.de>
2023-11-26 07:30:31 +0100

    fix working now

--- src/main/java/de/MCmoderSD/main/Main.java
+++ src/main/java/de/MCmoderSD/main/Main.java
@@ -3,14 +3,35 @@ package de.MCmoderSD.main;
 import de.MCmoderSD.utilities.json.JsonNode;
 import de.MCmoderSD.utilities.json.JsonUtility;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
         JsonUtility jsonUtility = new JsonUtility();
 
-        JsonNode jsonNode = jsonUtility.load("/languages/en.json");
+        ArrayList<String> list = new ArrayList<>();
 
-        System.out.println(jsonNode.get("title").asText());
-        System.out.println(jsonNode.get("join").asText());
+        JsonNode jsonNode = jsonUtility.load("/languages/en.json");
+        list.add(jsonNode.get("title").asText());
+        list.add(jsonNode.get("invalidMove").asText());
+        list.add(jsonNode.get("invalidObstacle").asText());
+        list.add(jsonNode.get("catWon").asText());
+        list.add(jsonNode.get("catcherWon").asText());
+        list.add(jsonNode.get("catIsNotOnMove").asText());
+        list.add(jsonNode.get("catcherIsNotOnMove").asText());
+        list.add(jsonNode.get("triesLeft").asText());
+        list.add(jsonNode.get("catIsOnMove").asText());
+        list.add(jsonNode.get("catcherIsOnMove").asText());
+        list.add(jsonNode.get("host").asText());
+        list.add(jsonNode.get("join").asText());
+        list.add(jsonNode.get("roomID").asText());
+        list.add(jsonNode.get("restart").asText());
+
+        String l = "";
+
+        for (String s : list) l = l + s;
+
+        System.out.println(l);
     }
 }
