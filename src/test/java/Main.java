3021c0c89e097db3f4dfcc7fb8806528b3e8d9a6
MCmoderSD <mail@mcmodersd.de>
2026-01-27 23:23:32 +0100

    Refactor Main class and README for improved readability

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -6,26 +6,23 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 @SuppressWarnings("ALL")
-public class Main {
+void main() {
 
-    public static void main(String[] args) {
+    // Create an instance of JsonUtility
+    JsonUtility jsonUtility = JsonUtility.getInstance();
 
-        // Create an instance of JsonUtility
-        JsonUtility jsonUtility = JsonUtility.getInstance();
+    try {
 
-        try {
+        // Load from resources folder
+        JsonNode resource = jsonUtility.load("/config.json");
 
-            // Load from resources folder
-            JsonNode resource = jsonUtility.load("/config.json");
+        // Load from URL
+        JsonNode url = jsonUtility.load("https://raw.githubusercontent.com/MCmoderSD/JsonUtility/refs/heads/master/src/test/resources/config.json");
 
-            // Load from URL
-            JsonNode url = jsonUtility.load("https://raw.githubusercontent.com/MCmoderSD/JsonUtility/refs/heads/master/src/test/resources/config.json");
+        // Load from absolute path
+        JsonNode absolute = jsonUtility.load("C:/Users/username/Desktop/config.json", true);
 
-            // Load from absolute path
-            JsonNode absolute = jsonUtility.load("C:/Users/username/Desktop/config.json", true);
-
-        } catch (IOException | URISyntaxException e) {
-            throw new RuntimeException("Failed to load JSON: " + e.getMessage(), e);
-        }
+    } catch (IOException | URISyntaxException e) {
+        throw new RuntimeException("Failed to load JSON: " + e.getMessage(), e);
     }
 }
